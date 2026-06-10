c3aa1966222a8f91ac968465b8ca088f8e3d6ef9
Katelyn Mei <katelynmei00@gmail.com>
2026-06-09 23:11:15 -0400

    update description

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -89,21 +89,21 @@ export const publicationData: Publication[] = [
     // if you have an image in public/images, you can use it like this:
     // imageUrl: "/images/publication-image.jpg"
   },
-  {
-    year: "2024",
-    conference: "ACM Conference on Fairness, Accountability, and Transparency (FAccT)",
-    title: "Careless Whisper: Speech-to-Text Hallucination Harms",
-    authors: "Allison Koenecke, Anna Seo Gyeong Choi, Katelyn X. Mei, Hilke Schellmann, Mona Sloane",
-    paperUrl: "https://arxiv.org/abs/2402.08021",
-    // codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
-    //bibtex: "https://arxiv.org/abs/2409.15476.bib",
-    tldr: "Hallucination in speech-to-text output in OpenAI Whisper.",
-    // imageUrl:
-    //   "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    // award: "🏆 Best Paper Award",
-    // if you have an image in public/images, you can use it like this:
-    // imageUrl: "/images/publication-image.jpg"
-  },
+  // {
+  //   year: "2024",
+  //   conference: "ACM Conference on Fairness, Accountability, and Transparency (FAccT)",
+  //   title: "Careless Whisper: Speech-to-Text Hallucination Harms",
+  //   authors: "Allison Koenecke, Anna Seo Gyeong Choi, Katelyn X. Mei, Hilke Schellmann, Mona Sloane",
+  //   paperUrl: "https://arxiv.org/abs/2402.08021",
+  //   // codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
+  //   //bibtex: "https://arxiv.org/abs/2409.15476.bib",
+  //   tldr: "Hallucination in speech-to-text output in OpenAI Whisper.",
+  //   // imageUrl:
+  //   //   "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //   // award: "🏆 Best Paper Award",
+  //   // if you have an image in public/images, you can use it like this:
+  //   // imageUrl: "/images/publication-image.jpg"
+  // },
   {
     year: "2023",
     conference: "ACM Conference on Fairness, Accountability, and Transparency (FAccT)",
